75c6cbe26c7e47224271be3cedfb9426540e0bae
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-06-28 19:43:49 -0700

    settings tweaking for js

--- custom/js.el
+++ custom/js.el
@@ -11,9 +11,6 @@
 
 (eval-after-load 'js2-mode
   '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
-;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
-(eval-after-load 'js
-  '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
 
 (eval-after-load 'json-mode
   '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
