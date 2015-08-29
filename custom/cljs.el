e07a8b939785909f7b15f4d16ab11036fdc25f66
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-08-28 20:26:44 -0700

    fix stdin prompt for ambly repl

--- custom/cljs.el
+++ custom/cljs.el
@@ -6,14 +6,14 @@
 
 (puthash "ambly" "(do (require '[cljs.repl :as repl])
 (require '[ambly.core :as ambly])
-(let [repl-env (ambly.core/repl-env)]
-  (cljs.repl/repl repl-env
-                  :watch \"src\"
-                  :watch-fn
-                  (fn []
-                      (cljs.repl/load-file repl-env
-                                           \"src/rfz/mobilenext/core.cljs\"))
-                  :analyze-path \"src\")))"  cljs-repls)
+(let [repl-env (ambly.core/repl-env :choose-first-discovered true)]
+  (cemerick.piggieback/cljs-repl repl-env
+                                 :watch \"src\"
+                                 :watch-fn
+                                 (fn []
+                                     (cljs.repl/load-file repl-env
+                                                          \"src/rfz/mobilenext/core.cljs\"))
+                                 :analyze-path \"src\")))" cljs-repls)
 
 (defun repls-with-cljs-prompt (repl-type)
   (interactive "scljs repl type: ")
