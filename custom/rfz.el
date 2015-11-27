cb1ae3177c3d99bf89aef1d25e7e374d7e5863d3
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-11-26 22:05:13 -0800

    rfz minor mode

--- custom/rfz.el
+++ custom/rfz.el
@@ -10,4 +10,28 @@
   (interactive)
   (grep (format "%s %s" "~/bin/rfzsearch" (symbol-at-point))))
 
-(global-set-key (kbd "C-7") 'rfzsearch-with-symbol-at-point)
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; RFZ cljs
+
+(defun rfz-cljs (build-name)
+  (interactive "srfz build?: ")
+  (save-window-excursion
+    (let ((default-directory "~/rfz/debtapp/app/"))
+      (async-shell-command (format  "lein rfz cljs %s auto" build-name))))
+  (with-current-buffer "*Async Shell Command*"
+    (rename-buffer (format "*cljsbuild* %s" build-name))))
+
+(define-minor-mode rfz-mode
+  "RFZ stuff"
+  :keymap (let ((kmap (make-sparse-keymap)))
+            (define-key kmap (kbd "H-1") #'rfzsearch-with-symbol-at-point)
+            (define-key kmap (kbd "H-2") #'rfz-cljs)
+            kmap))
+
+(dir-locals-set-class-variables 'rfz
+                                '((nil . ((eval . (rfz-mode))))))
+
+(dir-locals-set-directory-class
+ "~/rfz/"  'rfz)
+
+(provide 'rfz-mode)
