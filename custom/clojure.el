7b68823c6ea16d41de6a182c2adf3b3fad380b82
Johann Bestowrous <johann.bestowrous@gmail.com>
2016-05-09 14:36:03 -0700

    eldoc mode

--- custom/clojure.el
+++ custom/clojure.el
@@ -75,7 +75,7 @@
 (require 'cider)
 
 (add-hook 'clojure-mode-hook 'cider-mode)
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-mode-hook 'eldoc-mode)
 
 (setq nrepl-hide-special-buffers nil)
 (setq cider-repl-pop-to-buffer-on-connect t)
